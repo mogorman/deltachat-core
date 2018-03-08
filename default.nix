a463624220a2bca385c91dd6b070d08f067625c8
Matthew O'Gorman <mog@rldn.net>
2018-03-07 23:54:38 -0500

    working build system now that i updated libetpan

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with (import <nixpkgs> {});
 stdenv.mkDerivation {
   name = "delta-chat-core";
-  buildInputs = [ meson ninja libetpan openssl sqlite zlib pkgconfig cyrus_sasl  bzip2 ];
+  buildInputs = [ libetpan meson ninja openssl sqlite zlib pkgconfig cyrus_sasl  bzip2 ];
   shellHook = ''
     export PS1="delta-chat> "
 '';
